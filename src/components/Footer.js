82f669879a43bfd92cc7a4b3d9e7eae57420aa9e
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2021-10-06 14:59:23 -0700

    Added the Footer with functional links and icons

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import github from "../images/github-brands.svg";
+import linkedin from "../images/linkedin-brands.svg";
+import stackoverflow from "../images/stack-overflow-brands.svg";
+
+function Footer(props) {
+    return (
+        <footer className="container-fluid fixed-bottom bg-light py-2 border-top border-2 ">
+            <a className="mx-2" rel="noreferrer" target="_blank" href="https://github.com/s-cabrera/">
+                <img src={github} alt="Github link" width="30px" height="30px"/>
+            </a>
+
+            <a className="mx-2" rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/stephanie-cabrera-809999139/">
+                <img src={linkedin} alt="LinkedIn Link" width="30px" height="30px" />
+            </a>
+
+            <a className="mx-2" rel="noreferrer" target="_blank" href="https://stackoverflow.com/users/15790246/stephanie-cabrera">
+                <img src={stackoverflow} alt="Stack Overflow link" width="30px" height="30px"/>
+            </a>
+        
+        </footer>
+    )
+}
+
+export default Footer;
