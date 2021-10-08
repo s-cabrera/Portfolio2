fb3dc1b599c646cd07e997e246c51e23b0377439
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2021-10-08 01:29:33 -0700

    Added styling to the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 //Imports
 import React from "react";
-import avatar from "../images/avatar.png";
+// import avatar from "../images/avatar.png";
 // import { Fragment } from 'react';
 // import {
 //   Navbar, NavbarBrand, Nav, NavItem, NavLink
@@ -15,8 +15,8 @@ function Header(props) {
                         {/* <img className="me-2 nav-brand" src={avatar} alt="" width="30" height="30"/> */}
                         Menu
                     </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarToggler">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -42,11 +42,11 @@ function Header(props) {
                     </div>
                 </div>
             </nav>
-            <section className="img-section">
-                <button type="button" class="btn"></button>
+            {/* <section className="img-section position-relative">
+                <button type="button" className="btn position-absolute top-5 start-0">Light</button>
                 <img src={avatar} alt="Avatar icon" />
                 <h2 className="text-shadow">Stephanie Cabrera</h2>
-            </section>
+            </section> */}
         </header>
     );
 }
