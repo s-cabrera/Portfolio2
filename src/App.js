ec032692762a3af6c26d3b1e23692f36c15faadd
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2021-10-08 01:35:10 -0700

    Added the components to render them; Added Styling several components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import AboutMe from "./components/AboutMe";
+import Contact from "./components/Contact";
+import Portfolio from "./components/Portfolio";
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <AboutMe />
+      <Contact/>
+      <Portfolio/>
       <Footer />
     </div>
   );
