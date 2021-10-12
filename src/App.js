73f4f51f9dddcc461913089651ada2f241bfef35
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2021-10-11 23:04:38 -0700

    Added Router and Route to load the correct components depending on the which navbar title is clicked

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
+//Imports
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import AboutMe from "./components/AboutMe";
 import Contact from "./components/Contact";
 import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <Header />
-      <AboutMe />
-      <Contact/>
-      <Portfolio/>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Route exact path="/">
+          <AboutMe />
+        </Route>
+        
+        <Route exact path="/contact">
+          <Contact/>
+        </Route>
+        
+        <Route exact path="/projects">
+          <Portfolio/>
+        </Route>
+
+        <Route exact path="/resume">
+          <Resume />
+        </Route>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
