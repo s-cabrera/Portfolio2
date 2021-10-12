6be88ad164f0b3e70e13b5add418916e0e1425a3
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2021-10-11 22:54:15 -0700

    Fixed styling to make the Components move consistent

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,30 +1,30 @@
 //Imports
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faStackOverflow } from "@fortawesome/free-brands-svg-icons";
 
 //Image Imports
-import picture from "../images/hiking_selfie_2.jpg"
-// import github from "../images/github-brands.svg";
+// import picture from "../images/hiking_selfie_2.jpg"
 // import pokemon_search from "../images/pokemon_search_demo.gif"
-// import anime_q from "../images/Anime-Q-Demo.gif"
-// import password_generator from "../images/password_generator_demo.gif"
-// import coding_quiz  from "../images/coding_quiz_demo.gif"
-// import weather_dashboard from "../images/weather_dashboard_demo.gif"
-
+import anime_q from "../images/Anime-Q-Demo.gif"
+import password_generator from "../images/password_generator_demo.gif"
+import coding_quiz  from "../images/coding_quiz_demo.gif"
+import weather_dashboard from "../images/weather_dashboard_demo.gif"
+import tech_blog from "../images/Tech_Blog_Demo.gif"
+import fitness_tracker from "../images/Fitness_Tracker_Demo.gif"
 
 function Portfolio(props) {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
+        <div className="container py-3">
+            <div className="row justify-content-center pb-3">
                 <div className="card project-card col col-10 col-lg-5 m-2">
                     <div className="card-body">
-                        <a href="https://s-cabrera.github.io/Pokemon_Search/" rel="noreferrer" target="_blank">
-                            <h2>Pokemon Search</h2>
-                            {/* <img className="box-shadow" src={pokemon_search} alt="Pokemon Search Demo" /> */}
-                            <img src={picture} className="img-fluid" alt="photo1" />
+                        <a href="https://secret-beach-04921.herokuapp.com/" rel="noreferrer" target="_blank">
+                            <h2>Fitness Tracker</h2>
+                            <img className="img-fluid box-shadow" src={fitness_tracker} alt="Fitness_Tracker_Demo" />
+                            {/* <img src={picture} className="img-fluid" alt="photo1" /> */}
                             {/* <i class="fab fa-github project-github" href="https://github.com/s-cabrera/Pokemon_Search" style={{cursor: "pointer"}}></i> */}
-                            <a href="https://github.com/s-cabrera/Pokemon_Search" className="project-github" style={{cursor: "pointer"}} rel="noreferrer" target="_blank" >
+                            <a href="https://github.com/s-cabrera/Fitness_Tracker" className="project-github" style={{cursor: "pointer"}} rel="noreferrer" target="_blank" >
                                 <FontAwesomeIcon icon={faGithub}/>
                             </a>
                         </a>
@@ -34,8 +34,8 @@ function Portfolio(props) {
                     <div className="card-body">
                         <a href="https://anime-q-list.herokuapp.com" rel="noreferrer" target="_blank">
                             <h2>Anime-Q</h2>
-                            {/* <img className="box-shadow" src={anime_q} alt="Anime-Q Demo"/> */}
-                            <img src={picture} className="img-fluid" alt="photo2" />
+                            <img className="img-fluid box-shadow" src={anime_q} alt="Anime-Q Demo"/>
+                            {/* <img src={picture} className="img-fluid" alt="photo2" /> */}
                             {/* <i class="fab fa-github project-github" href="https://github.com/s-cabrera/Phase2_Project" style={{cursor: "pointer"}}></i> */}
                             <a href="https://github.com/s-cabrera/Phase2_Project" className="project-github" style={{cursor: "pointer"}} rel="noreferrer" target="_blank" >
                                 <FontAwesomeIcon icon={faGithub}/>
@@ -47,8 +47,8 @@ function Portfolio(props) {
                     <div className="card-body">
                         <a href="https://s-cabrera.github.io/Password_Generator/" rel="noreferrer" target="_blank">
                             <h2>Password Generator</h2>
-                            {/* <img className="box-shadow" src={password_generator} alt="Password Generator Demo" />  */}
-                            <img src={picture} className="img-fluid" alt="photo3" />
+                            <img className="img-fluid box-shadow" src={password_generator} alt="Password Generator Demo" /> 
+                            {/* <img src={picture} className="img-fluid" alt="photo3" /> */}
                             {/* <button className="fab fa-github project-github" href="https://github.com/s-cabrera/Password_Generator" style={{cursor: "pointer"}} /> */}
                             <a href="https://github.com/s-cabrera/Password_Generator" className="project-github" style={{cursor: "pointer"}} rel="noreferrer" target="_blank" >
                                 <FontAwesomeIcon icon={faGithub}/>
@@ -60,8 +60,8 @@ function Portfolio(props) {
                     <div className="card-body">
                         <a href="https://s-cabrera.github.io/Code_Quiz/" rel="noreferrer" target="_blank">
                         <h2>Coding Quiz</h2>
-                            {/* <img className="box-shadow" src={coding_quiz} alt="Coding Quiz Demo" />*/}
-                            <img src={picture} className="img-fluid" alt="photo3" />
+                            <img className="img-fluid box-shadow" src={coding_quiz} alt="Coding Quiz Demo" />
+                            {/* <img src={picture} className="img-fluid" alt="photo3" /> */}
                             {/* <i class="fab fa-github project-github" href="https://github.com/s-cabrera/Code_Quiz" style={{cursor: "pointer"}}></i> */}
                             <a href="https://github.com/s-cabrera/Code_Quiz" className="project-github" style={{cursor: "pointer"}} rel="noreferrer" target="_blank" >
                                 <FontAwesomeIcon icon={faGithub}/>
@@ -72,25 +72,25 @@ function Portfolio(props) {
                 <div className="card project-card col col-10 col-lg-5 m-2">
                     <div className="card-body">
                         <a href="https://s-cabrera.github.io/Weather_Dashboard/" rel="noreferrer" target="_blank">
-                        <h2>Weather Dashboard</h2>
-                            {/* <img className="box-shadow" src={weather_dashboard} alt="Weather Dashboard Demo" /> */}
-                            <img src={picture} className="img-fluid" alt="photo3" />
+                            <h2>Weather Dashboard</h2>
+                            <img className="img-fluid box-shadow" src={weather_dashboard} alt="Weather Dashboard Demo" />
+                            {/* <img src={picture} className="img-fluid" alt="photo3" /> */}
                             {/* <i class="fab fa-github project-github" href="https://github.com/s-cabrera/Weather_Dashboard" style={{cursor: "pointer"}}></i> */}
                             <a href="https://github.com/s-cabrera/Weather_Dashboard" className="project-github" style={{cursor: "pointer"}} rel="noreferrer" target="_blank" >
-                                <FontAwesomeIcon icon={faGithub}/>
+                                <FontAwesomeIcon icon={faLinkedin}/>
                             </a>
                         </a>
                     </div>
                 </div>
                 <div className="card project-card col col-10 col-lg-5 m-2">
                     <div className="card-body">
-                        <a href="https://s-cabrera.github.io/Password_Generator/" rel="noreferrer" target="_blank">
-                            <h2>ADD NEW PROJECT HERE</h2>
-                            {/* <img class="box-shadow" src={} alt="Pokemon Search Demo" /> */}
-                            <img src={picture} className="img-fluid" alt="photo3" />
+                        <a href="https://shielded-dawn-70616.herokuapp.com/" rel="noreferrer" target="_blank">
+                            <h2>Tech Blog</h2>
+                            <img class="box-shadow" src={tech_blog} alt="Pokemon Search Demo" />
+                            {/* <img src={picture} className="img-fluid" alt="photo3" /> */}
                             {/* <i class="fab fa-github project-github" href="https://github.com/s-cabrera/Pokemon_Search" style={{cursor: "pointer"}}></i> */}
-                            <a href="https://github.com/s-cabrera/Pokemon_Search" className="project-github" style={{cursor: "pointer"}} rel="noreferrer" target="_blank" >
-                                <FontAwesomeIcon icon={faGithub}/>
+                            <a href="https://github.com/s-cabrera/Tech-Blog" className="project-github" style={{cursor: "pointer"}} rel="noreferrer" target="_blank" >
+                                <FontAwesomeIcon icon={faStackOverflow}/>
                             </a>
                         </a>
                     </div>
